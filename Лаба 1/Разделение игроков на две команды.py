073bbb2cd7330066e6352075d7e13f65bc370f1a
KiraMaslova03 <149327101+KiraMaslova03@users.noreply.github.com>
2023-10-29 21:59:54 +0300

    Лаба 1

---
+++ Лаба 1/Разделение игроков на две команды.py
@@ -0,0 +1,10 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+kolvo = len(list_players)
+
+team_one = list_players [:3]
+team_two = list_players [3:]
+
+print(team_one)
+print(team_two)
+
+# TODO Разделите участников на две команды
